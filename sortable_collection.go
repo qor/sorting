96c01fbf2ff77b6ebbe90ec8a2717d4f501fa858
Jinzhu <wosmvp@gmail.com>
2016-05-05 12:01:06 +0800

    Add sortable collection

---
+++ sortable_collection.go
@@ -0,0 +1,73 @@
+package sorting
+
+import (
+	"database/sql/driver"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"reflect"
+
+	"github.com/jinzhu/gorm"
+)
+
+type SortableCollection struct {
+	PrimaryField string
+	PrimaryKeys  []string
+}
+
+func (sortableCollection *SortableCollection) Scan(value interface{}) error {
+	switch values := value.(type) {
+	case []string:
+		sortableCollection.PrimaryKeys = values
+	case []byte:
+		return json.Unmarshal(values, sortableCollection)
+	default:
+		return errors.New("unsupported driver -> Scan pair for MediaLibrary")
+	}
+
+	return nil
+}
+
+func (sortableCollection SortableCollection) Value() (driver.Value, error) {
+	results, err := json.Marshal(sortableCollection)
+	return string(results), err
+}
+
+func (sortableCollection SortableCollection) Sort(results interface{}) error {
+	values := reflect.ValueOf(results)
+	if values.Kind() != reflect.Ptr && values.Elem().Kind() != reflect.Slice {
+		return errors.New("invalid type")
+	}
+
+	var (
+		scope            = gorm.Scope{Value: results}
+		primaryFieldName = scope.PrimaryField().Name
+		indirectValues   = values.Elem()
+		sliceType        = indirectValues.Type()
+		slice            = reflect.MakeSlice(sliceType, 0, 0)
+		slicePtr         = reflect.New(sliceType)
+		orderedMap       = map[int]bool{}
+	)
+
+	slicePtr.Elem().Set(slice)
+	for _, primaryKey := range sortableCollection.PrimaryKeys {
+		for i := 0; i < indirectValues.Len(); i++ {
+			value := indirectValues.Index(i)
+			field := value.FieldByName(primaryFieldName)
+			if fmt.Sprint(field.Interface()) == primaryKey {
+				slicePtr.Elem().Set(reflect.Append(slicePtr.Elem(), value))
+				orderedMap[i] = true
+			}
+		}
+	}
+
+	for i := 0; i < indirectValues.Len(); i++ {
+		if _, ok := orderedMap[i]; !ok {
+			slicePtr.Elem().Set(reflect.Append(slicePtr.Elem(), indirectValues.Index(i)))
+		}
+	}
+
+	values.Elem().Set(slicePtr.Elem())
+
+	return nil
+}
---
+++ sortable_collection_test.go
@@ -0,0 +1,58 @@
+package sorting_test
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+
+	"github.com/jinzhu/gorm"
+	"github.com/qor/sorting"
+)
+
+type ColorVariation struct {
+	gorm.Model
+	Code string
+}
+
+func checkOrder(results interface{}, order []string) error {
+	values := reflect.Indirect(reflect.ValueOf(results))
+	for idx, o := range order {
+		value := values.Index(idx)
+		primaryValue := fmt.Sprint(value.FieldByName("ID").Interface())
+		if primaryValue != o {
+			return fmt.Errorf("#%v of values's primary key is %v, but should be %v", idx+1, primaryValue, o)
+		}
+	}
+	return nil
+}
+
+func TestSort(t *testing.T) {
+	colorVariations := &[]ColorVariation{
+		{Model: gorm.Model{ID: 1}, Code: "1"},
+		{Model: gorm.Model{ID: 2}, Code: "2"},
+		{Model: gorm.Model{ID: 3}, Code: "3"},
+	}
+
+	collectionSorting := sorting.SortableCollection{PrimaryKeys: []string{"3", "1", "2"}}
+	collectionSorting.Sort(colorVariations)
+
+	if err := checkOrder(colorVariations, []string{"3", "1", "2"}); err != nil {
+		t.Error(err)
+	}
+}
+
+func TestSortWithSomePrimaryKeys(t *testing.T) {
+	colorVariations := &[]ColorVariation{
+		{Model: gorm.Model{ID: 1}, Code: "1"},
+		{Model: gorm.Model{ID: 2}, Code: "2"},
+		{Model: gorm.Model{ID: 3}, Code: "3"},
+		{Model: gorm.Model{ID: 4}, Code: "4"},
+	}
+
+	collectionSorting := sorting.SortableCollection{PrimaryKeys: []string{"3", "1"}}
+	collectionSorting.Sort(colorVariations)
+
+	if err := checkOrder(colorVariations, []string{"3", "1", "2", "4"}); err != nil {
+		t.Error(err)
+	}
+}
