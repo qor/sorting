85fcb1fb9fb330e43173e303da0f5e42d1b05575
Jinzhu <wosmvp@gmail.com>
2016-05-06 12:13:25 +0800

    Basic implementation of Sorter

--- sortable_collection.go
+++ sortable_collection.go
@@ -12,6 +12,7 @@ import (
 	"github.com/qor/admin"
 	"github.com/qor/qor"
 	"github.com/qor/qor/resource"
+	"github.com/qor/qor/utils"
 )
 
 type SortableCollection struct {
@@ -39,58 +40,71 @@ func (sortableCollection SortableCollection) Value() (driver.Value, error) {
 
 func (sortableCollection SortableCollection) Sort(results interface{}) error {
 	values := reflect.ValueOf(results)
-	if values.Kind() != reflect.Ptr && values.Elem().Kind() != reflect.Slice {
+	if values.Kind() != reflect.Ptr && reflect.Indirect(values).Kind() != reflect.Slice {
 		return errors.New("invalid type")
 	}
 
-	var (
-		scope            = gorm.Scope{Value: results}
-		primaryFieldName = scope.PrimaryField().Name
-		indirectValues   = values.Elem()
-		sliceType        = indirectValues.Type()
-		slice            = reflect.MakeSlice(sliceType, 0, 0)
-		slicePtr         = reflect.New(sliceType)
-		orderedMap       = map[int]bool{}
-	)
+	scope := gorm.Scope{Value: results}
+	if primaryField := scope.PrimaryField(); primaryField != nil {
+		var (
+			primaryFieldName = primaryField.Name
+			indirectValues   = values.Elem()
+			sliceType        = indirectValues.Type()
+			slice            = reflect.MakeSlice(sliceType, 0, 0)
+			slicePtr         = reflect.New(sliceType)
+			orderedMap       = map[int]bool{}
+		)
 
-	slicePtr.Elem().Set(slice)
-	for _, primaryKey := range sortableCollection.PrimaryKeys {
-		for i := 0; i < indirectValues.Len(); i++ {
-			value := indirectValues.Index(i)
-			field := value.FieldByName(primaryFieldName)
-			if fmt.Sprint(field.Interface()) == primaryKey {
-				slicePtr.Elem().Set(reflect.Append(slicePtr.Elem(), value))
-				orderedMap[i] = true
+		slicePtr.Elem().Set(slice)
+		for _, primaryKey := range sortableCollection.PrimaryKeys {
+			for i := 0; i < indirectValues.Len(); i++ {
+				value := indirectValues.Index(i)
+				field := value.FieldByName(primaryFieldName)
+				if fmt.Sprint(field.Interface()) == primaryKey {
+					slicePtr.Elem().Set(reflect.Append(slicePtr.Elem(), value))
+					orderedMap[i] = true
+				}
 			}
 		}
-	}
 
-	for i := 0; i < indirectValues.Len(); i++ {
-		if _, ok := orderedMap[i]; !ok {
-			slicePtr.Elem().Set(reflect.Append(slicePtr.Elem(), indirectValues.Index(i)))
+		for i := 0; i < indirectValues.Len(); i++ {
+			if _, ok := orderedMap[i]; !ok {
+				slicePtr.Elem().Set(reflect.Append(slicePtr.Elem(), indirectValues.Index(i)))
+			}
 		}
-	}
 
-	values.Elem().Set(slicePtr.Elem())
+		values.Elem().Set(slicePtr.Elem())
+	}
 
 	return nil
 }
 
-func (sortableCollection SortableCollection) ConfigureQorMeta(metaor resource.Metaor) error {
+func (sortableCollection *SortableCollection) ConfigureQorMeta(metaor resource.Metaor) {
 	if meta, ok := metaor.(*admin.Meta); ok {
-		name := strings.TrimSuffix(meta.GetName(), "Sorter")
-		res := meta.GetBaseResource().(*admin.Resource)
-		sortableMeta := res.GetMeta(name)
+		var (
+			name         = strings.TrimSuffix(meta.GetName(), "Sorter")
+			res          = meta.GetBaseResource().(*admin.Resource)
+			sortableMeta = res.GetMeta(name)
+		)
 
-		setter := sortableMeta.GetSetter()
-		sortableMeta.SetSetter(func(record interface{}, metaValue *resource.MetaValue, context *qor.Context) {
-			setter(record, metaValue, context)
-		})
+		if sortableMeta != nil && (sortableMeta.Type == "select_many" || sortableMeta.Type == "collection_edit") {
+			sortableMeta.Type = "sortable_" + sortableMeta.Type
 
-		valuer := sortableMeta.GetValuer()
-		sortableMeta.SetValuer(func(record interface{}, context *qor.Context) interface{} {
-			return valuer(record, context)
-		})
+			setter := sortableMeta.GetSetter()
+			sortableMeta.SetSetter(func(record interface{}, metaValue *resource.MetaValue, context *qor.Context) {
+				primaryKeys := utils.ToArray(metaValue.Value)
+				reflectValue := reflect.Indirect(reflect.ValueOf(record))
+				reflectValue.FieldByName(meta.GetName()).Set(reflect.ValueOf(primaryKeys))
+				setter(record, metaValue, context)
+			})
+
+			valuer := sortableMeta.GetValuer()
+			sortableMeta.SetValuer(func(record interface{}, context *qor.Context) interface{} {
+				results := valuer(record, context)
+				reflectValue := reflect.Indirect(reflect.ValueOf(record))
+				reflectValue.FieldByName(meta.GetName()).Interface().(SortableCollection).Sort(results)
+				return results
+			})
+		}
 	}
-	return nil
 }
