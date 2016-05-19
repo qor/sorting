58c9bcf9268cb4f8f89c1fea0f5162c658e47148
jason weng <wengjianm@gmail.com>
2016-05-19 20:05:26 +0800

    add sortable assets

--- views/assets/javascripts/sorting-sortable-select-many/sorting-sortable-select-many.js
+++ views/assets/javascripts/sorting-sortable-select-many/sorting-sortable-select-many.js
@@ -15,15 +15,21 @@
 
   var Mustache = window.Mustache;
   var Sortable = window.Sortable;
+  var _ = window._;
   var NAMESPACE = 'qor.chooser.sortable';
   var EVENT_ENABLE = 'enable.' + NAMESPACE;
+  var EVENT_CLICK = 'click.' + NAMESPACE;
   var EVENT_DISABLE = 'disable.' + NAMESPACE;
   var CLASS_MULTI = '.chosen-container-multi';
   var CLASS_CHOSE = '.search-choice';
+  var CLASS_CHOSE_CONTAINER = '.chosen-container';
   var CLASS_SORTABLE_BODY = '.qor-dragable';
   var CLASS_SORTABLE = '.qor-dragable__list';
   var CLASS_SORTABLE_HANDLE = '.qor-dragable__list-handle';
   var CLASS_SORTABLE_DELETE = '.qor-dragable__list-delete';
+  var CLASS_SORTABLE_DATA = '.qor-dragable__list-data';
+  var CLASS_SORTABLE_BUTTON_ADD = '.qor-dragable__button-add';
+  var IS_LOADED = 'load-sortable-select-many';
 
   function QorChooserSortable(element, options) {
     this.$element = $(element);
@@ -38,14 +44,18 @@
       var $this = this.$element;
       var $parent = $this.parents(CLASS_SORTABLE_BODY);
       var self = this;
+
+      this.$selector = $parent.find(CLASS_SORTABLE_DATA);
       this.$sortableList = $parent.find(CLASS_SORTABLE);
       this.$parent = $parent;
 
       var sortEle = $parent.find(CLASS_SORTABLE)[0];
-      Sortable.create(sortEle, {
+
+      this.sortable = Sortable.create(sortEle, {
           animation: 150,
           handle: CLASS_SORTABLE_HANDLE,
           filter: CLASS_SORTABLE_DELETE,
+          dataIdAttr: 'data-index',
 
           onFilter: function (e){
             // TODO
@@ -55,22 +65,8 @@
             $ele.remove();
             self.removeItems(eleIndex);
           },
-          onUpdate: function (e){
-            var newIndex = e.newIndex;
-            var $ele = $(e.item);
-            var eleIndex = $ele.data('index');
-            var $targetOption = $this.find('option[data-index="' + eleIndex + '"]');
-
-            $targetOption.remove();
-
-            if (newIndex == 0){
-              $this.prepend($targetOption);
-            } else {
-              $this.find('option:nth-child(' + newIndex + ')').after($targetOption);
-            }
-
-
-
+          onUpdate: function (){
+            self.renderOption();
           }
       });
 
@@ -85,8 +81,9 @@
       $this.on('chosen:ready', function (e,chosen) {
 
         $(chosen.chosen.search_field).attr('placeholder',this.$element.data('placeholder'));
-
         $parent.find(CLASS_CHOSE).hide();
+        $parent.find(CLASS_CHOSE_CONTAINER).hide();
+
       }.bind(this));
 
       $this.chosen({
@@ -99,6 +96,7 @@
       .on('change', function (e,params) {
         var $target = $(e.target);
         var $chosenMulti = $target.next(CLASS_MULTI);
+        var selected = params.selected;
 
         if (!$chosenMulti.size()){
           return;
@@ -106,16 +104,43 @@
 
         $parent.find(CLASS_CHOSE).hide();
 
-        if (params.selected){
-          this.addItems(params.selected);
+        if (selected){
+          this.addItems(selected);
         }
 
       }.bind(this));
 
+      this.bind();
+
+    },
+
+    bind: function () {
+      this.$parent.on(EVENT_CLICK, CLASS_SORTABLE_BUTTON_ADD,  this.show.bind(this));
+    },
+
+    unbind: function () {
+      this.$parent.off(EVENT_CLICK, CLASS_SORTABLE_BUTTON_ADD, this.show);
+    },
+
+    show: function () {
+      this.$parent.find(CLASS_CHOSE_CONTAINER).show().find('.search-field input').click();
+      this.$parent.find(CLASS_SORTABLE_BUTTON_ADD).hide();
     },
 
     renderItem: function (data) {
       return Mustache.render(QorChooserSortable.LIST_HTML, data);
+    },
+
+    renderOption: function () {
+      var indexArr = this.sortable.toArray();
+      var $selector = this.$selector;
+
+      $selector.empty();
+
+      _.each(indexArr,function(id){
+        $selector.append( Mustache.render(QorChooserSortable.OPTION_HTML, ({'value': id})));
+      });
+
     },
 
     removeItems: function (index) {
@@ -124,6 +149,8 @@
       var optionArrayIndex = $this.find('option').index(targetOption);
 
       this.$parent.find(CLASS_CHOSE).find('[data-option-array-index="' + optionArrayIndex + '"]').click();
+
+      this.renderOption();
     },
 
     addItems: function (index) {
@@ -131,16 +158,20 @@
       var data = $this.find('option[value="' + index + '"]').data();
 
       this.$sortableList.append(this.renderItem(data));
+      this.renderOption();
     },
 
     destroy: function () {
+      this.sortable.destroy();
+      this.unbind();
       this.$element.chosen('destroy').removeData(NAMESPACE);
     }
   };
 
   QorChooserSortable.DEFAULTS = {};
 
-  QorChooserSortable.LIST_HTML = '<li data-index=[[index]] data-value=[[value]]><span>[[value]]</span><i class="material-icons qor-dragable__list-handle">swap_vert</i><i class="material-icons qor-dragable__list-delete">clear</i></li>';
+  QorChooserSortable.LIST_HTML = '<li data-index=[[index]] data-value=[[value]]><span>[[value]]</span><i class="material-icons qor-dragable__list-delete">clear</i><i class="material-icons qor-dragable__list-handle">drag_handle</i></li>';
+  QorChooserSortable.OPTION_HTML = '<option selected value=[[value]]></option>';
 
   QorChooserSortable.plugin = function (options) {
     return this.each(function () {
@@ -168,6 +199,12 @@
 
   $(function () {
     var selector = 'select[data-toggle="qor.chooser.sortable"]';
+
+    if ($('body').data(IS_LOADED)) {
+      return;
+    }
+
+    $('body').data(IS_LOADED,'true');
 
     $(document).
       on(EVENT_DISABLE, function (e) {
