3d35e727b8e6a0516adcbc813b58e30245b217e0
jasonweng <wengjianm@gmail.com>
2017-05-13 15:37:09 +0800

    update bottom sheet sortable JS

--- views/assets/javascripts/sorting-sortable-select-many/2.sorting-sortable-select-many.js
+++ views/assets/javascripts/sorting-sortable-select-many/2.sorting-sortable-select-many.js
@@ -19,7 +19,6 @@
     var EVENT_CLICK = 'click.' + NAMESPACE;
     var EVENT_DISABLE = 'disable.' + NAMESPACE;
     var CLASS_CHOSE = '.select2-selection__choice';
-    var CLASS_CHOSE_REMOVE = '.select2-selection__choice__remove';
     var CLASS_CHOSE_CONTAINER = '.select2-container';
     var CLASS_CHOSE_INPUT = '.select2-search__field';
     var CLASS_SORTABLE_BODY = '.qor-dragable';
@@ -29,11 +28,9 @@
     var CLASS_SORTABLE_DATA = '.qor-dragable__list-data';
     var CLASS_SORTABLE_BUTTON_ADD = '.qor-dragable__button-add';
     var CLASS_BOTTOMSHEETS = '.qor-bottomsheets';
-    var CLASS_PARENT = '.qor-dragable';
-    var CLASS_SELECT_FIELD = '.qor-dragable__list';
     var IS_LOADED = 'sortable-select-many-loaded';
     var CLASS_MANY = 'qor-bottomsheets__select-many';
-    var CLASS_DELETED_ITEM = 'qor-selected-many__deleted';
+    var CLASS_SELECTED = 'is_selected';
 
     function QorChooserSortable(element, options) {
         this.$element = $(element);
@@ -94,26 +91,30 @@
                 };
             }
 
-            $this.on('change', function() {
+            if ($this.is('select')) {
 
-                    setTimeout(function() {
-                        $parent.find(CLASS_CHOSE).hide();
-                    }, 1);
+                $this.on('change', function() {
 
-                    $(CLASS_CHOSE_INPUT).attr('placeholder', placeholderText);
-                })
-                .on('select2:select', function(e) {
-                    self.addItems(e.params.data);
-                })
-                .on('select2:unselect', function(e) {
-                    self.removeItems(e.params.data);
-                });
+                        setTimeout(function() {
+                            $parent.find(CLASS_CHOSE).hide();
+                        }, 1);
 
-            $this.select2(option);
+                        $(CLASS_CHOSE_INPUT).attr('placeholder', placeholderText);
+                    })
+                    .on('select2:select', function(e) {
+                        console.log(e.params.data);
+                        self.addItems(e.params.data);
+                    })
+                    .on('select2:unselect', function(e) {
+                        self.removeItems(e.params.data);
+                    });
 
-            $parent.find(CLASS_CHOSE_CONTAINER).hide();
-            $parent.find(CLASS_CHOSE).hide();
-            $(CLASS_CHOSE_INPUT).attr('placeholder', placeholderText);
+                $this.select2(option);
+
+                $parent.find(CLASS_CHOSE_CONTAINER).hide();
+                $parent.find(CLASS_CHOSE).hide();
+                $(CLASS_CHOSE_INPUT).attr('placeholder', placeholderText);
+            }
 
             this.bind();
         },
@@ -126,85 +127,87 @@
             this.$parent.off(EVENT_CLICK, CLASS_SORTABLE_BUTTON_ADD, this.show);
         },
 
-        show: function(e) {
-          var $this = $(e.target).parent('.qor-dragable__button-add');
-          if ($this.attr('data-selectmany-url')) {
-              var data = $this.data();
+        show: function() {
+            var $btn = this.$parent.find('.qor-dragable__button-add');
 
-              this.BottomSheets = $body.data('qor.bottomsheets');
-              this.bottomsheetsData = data;
+            if ($btn.attr('data-selectmany-url')) {
+                var data = $btn.data();
 
-              this.$selector = data.selectId ? $(data.selectId) : $this.closest(CLASS_PARENT).find('select');
-              this.$selectFeild = this.$selector.closest(CLASS_PARENT).find(CLASS_SELECT_FIELD);
+                this.BottomSheets = $body.data('qor.bottomsheets');
+                data.url = data.selectmanyUrl;
+                this.BottomSheets.open(data, this.handleBottomSelect.bind(this));
 
-              // select many templates
-              this.SELECT_MANY_SELECTED_ICON = $('[name="select-many-selected-icon"]').html();
-              this.SELECT_MANY_UNSELECTED_ICON = $('[name="select-many-unselected-icon"]').html();
-              this.SELECT_MANY_HINT = $('[name="select-many-hint"]').html();
-              this.SELECT_MANY_TEMPLATE = $('[name="select-many-template"]').html();
-
-              data.url = data.selectmanyUrl;
-
-              this.BottomSheets.open(data, this.handleBottomSelect.bind(this));
-          } else {
-              var $container = this.$parent.find(CLASS_CHOSE_CONTAINER);
-
-              $container.show();
-              this.$parent.find(CLASS_SORTABLE_BUTTON_ADD).hide();
-              setTimeout(function() {
-                  $container.find(CLASS_CHOSE_INPUT).click();
-              }, 100);
-          }
-        },
-
-        handleBottomSelect: function () {
-          var $bottomsheets = $(CLASS_BOTTOMSHEETS),
-          options = {
-            onSelect: this.onSelectResults.bind(this),  // render selected item after click item lists
-            onSubmit: this.onSubmitResults.bind(this)   // render new items after new item form submitted
-          };
-
-          $bottomsheets.qorSelectCore(options).addClass(CLASS_MANY);
-          this.initItems();
-        },
-
-        onSelectResults: function (data) {
-          if ($(CLASS_SORTABLE).find('li[data-index="' + data.primaryKey + '"]').size() == 0) {
-            this.addItems(data);
-          } else {
-            this.removeItems(data);
-          }
-        },
-
-        onSubmitResults: function (data) {
-          this.addItems(data, true);
-        },
-
-        initItems: function () {
-          var $tr = $(CLASS_BOTTOMSHEETS).find('tbody tr'),
-              selectedIconTmpl = this.SELECT_MANY_SELECTED_ICON,
-              unSelectedIconTmpl = this.SELECT_MANY_UNSELECTED_ICON,
-              selectedIDs = [],
-              primaryKey,
-              $selectedItems = this.$selectFeild.find('[data-primary-key]').not('.' + CLASS_DELETED_ITEM);
-
-          $selectedItems.each(function () {
-            selectedIDs.push($(this).data().primaryKey);
-          });
-
-          $tr.each(function () {
-            var $this = $(this),
-                $td = $this.find('td:first');
-
-            primaryKey = $this.data().primaryKey;
-
-            if (selectedIDs.indexOf(primaryKey) !='-1') {
-              $this.addClass(CLASS_SELECTED);
-              $td.append(selectedIconTmpl);
             } else {
-              $td.append(unSelectedIconTmpl);
+                var $container = this.$parent.find(CLASS_CHOSE_CONTAINER);
+
+                $container.show();
+                this.$parent.find(CLASS_SORTABLE_BUTTON_ADD).hide();
+                setTimeout(function() {
+                    $container.find(CLASS_CHOSE_INPUT).click();
+                }, 100);
+
             }
-          });
+        },
+
+        handleBottomSelect: function() {
+            var $bottomsheets = $(CLASS_BOTTOMSHEETS),
+                options = {
+                    onSelect: this.onSelectResults.bind(this), // render selected item after click item lists
+                    onSubmit: this.onSubmitResults.bind(this) // render new items after new item form submitted
+                };
+
+            $bottomsheets.qorSelectCore(options).addClass(CLASS_MANY);
+            this.initItems();
+        },
+
+        onSelectResults: function(data) {
+
+            // Handle data for sortable
+            // data.value = data.Name || data.text || data.Text || data.Title || data.Code || data.Id || data.ID || data[Object.keys(data)[0]];
+            let remoteDataPrimaryKey = this.$element.data('remote-data-primary-key'),
+                obj = {};
+
+            if (remoteDataPrimaryKey) {
+                obj.id = data[remoteDataPrimaryKey];
+            } else {
+                obj.id = data.Id || data.ID || data.primaryKey;
+            }
+
+            obj.value = data.Name || data.text || data.Text || data.Title || data.Code || obj.id;
+
+            if (!$(CLASS_SORTABLE).find('li[data-index="' + obj.id + '"]').length) {
+                this.addItems(obj);
+            } else {
+                this.removeItems(obj);
+            }
+        },
+
+        onSubmitResults: function(data) {
+            this.addItems(data, true);
+        },
+
+        initItems: function() {
+            var $tr = $(CLASS_BOTTOMSHEETS).find('tbody tr'),
+                selectedIconTmpl = $('[name="select-many-selected-icon"]').html(),
+                selectedIDs = [],
+                primaryKey,
+                $selectedItems = this.$sortableList.find('[data-index]');
+
+            $selectedItems.each(function() {
+                selectedIDs.push($(this).data('index'));
+            });
+
+            $tr.each(function() {
+                var $this = $(this),
+                    $td = $this.find('td:first');
+
+                primaryKey = $this.data().primaryKey;
+
+                if (selectedIDs.indexOf(primaryKey) != '-1') {
+                    $this.addClass(CLASS_SELECTED);
+                    $td.append(selectedIconTmpl);
+                }
+            });
         },
 
         renderItem: function(data) {
@@ -225,22 +228,21 @@
         },
 
         removeItems: function(data) {
-            $(CLASS_SORTABLE).find('li[data-index="' + data.primaryKey + '"]').remove();
+
+            $(CLASS_SORTABLE).find('li[data-index="' + data.id + '"]').remove();
             this.renderOption();
         },
 
-        removeItemsFromList: function(index) {
+        removeItemsFromList: function() {
             this.renderOption();
         },
 
         addItems: function(data, isNewData) {
-            data.id = data.Id || data.ID || data[Object.keys(data)[0]];
-            data.value = data.Name || data.text || data.Text || data.Title || data.Code || data.Id || data.ID || data[Object.keys(data)[0]];
             this.$sortableList.append(this.renderItem(data));
             this.renderOption();
 
             if (isNewData) {
-              this.BottomSheets.hide();
+                this.BottomSheets.hide();
             }
         },
 
@@ -253,7 +255,7 @@
 
     QorChooserSortable.DEFAULTS = {};
 
-    QorChooserSortable.LIST_HTML = '<li data-index="[[primaryKey]]" data-value="[[value]]"><span>[[value]]</span><div><i class="material-icons qor-dragable__list-delete">clear</i><i class="material-icons qor-dragable__list-handle">drag_handle</i></div></li>';
+    QorChooserSortable.LIST_HTML = '<li data-index="[[id]]" data-value="[[value]]"><span>[[value]]</span><div><i class="material-icons qor-dragable__list-delete">clear</i><i class="material-icons qor-dragable__list-handle">drag_handle</i></div></li>';
 
     QorChooserSortable.OPTION_HTML = '<option selected value="[[value]]"></option>';
 
@@ -279,7 +281,7 @@
     };
 
     $(function() {
-        var selector = 'select[data-toggle="qor.chooser.sortable"]';
+        var selector = '[data-toggle="qor.chooser.sortable"]';
 
         if ($('body').data(IS_LOADED)) {
             return;
